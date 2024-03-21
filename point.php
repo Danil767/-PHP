bcb416a007c5938b70ce5187be4079df17055894
Danil767 <danil.an.2024@mail.ru>
2024-03-21 21:20:12 +0600

    php new commit

---
+++ point.php
@@ -0,0 +1,10 @@
+<!-- ООП: Классы и объекты -->
+
+ <?php
+class Point
+{
+  public $x;
+  public $y;
+  public static $num = 120; // Статическая переменная (можно вывести echo Point::$num;)
+}
+?> 
---
+++ point1.php
@@ -0,0 +1,6 @@
+<?php
+class Point1
+{
+  public $x;
+  public $y;
+}
